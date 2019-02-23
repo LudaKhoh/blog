33568cf4c00c02021b406b86bf18d67f61a7dee5
LudaKhoh <ludahoh19@gmail.com>
2019-02-23 18:03:34 +0200

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from blog import urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
